723908625aa5472656c80bec022744e90ee518f2
Andrew Butler <abutler3@gmail.com>
2015-01-24 16:55:41 -0600

    add user login and logout

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,24 +3,59 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('LoginController', ['$scope', '$location', function($scope, $location) {
+  controller('LoginController', ['$scope', '$firebaseSimpleLogin', '$location', '$rootScope', function($scope, $firebaseSimpleLogin, $location, $rootScope) {
     $scope.name = 'Drew';
+    var ref = new Firebase('https://domusstatic.firebaseio.com/');
+    var auth = $firebaseSimpleLogin(ref);
+    $scope.user = { email: '', password: '' };
 
     $scope.login = function() {
-      $location.path('/meetings');
+      auth.$login('password', $scope.user).then(function(data) {
+        console.log(data);
+        $location.path('/meetings');
+      });
     }
+
+    $scope.logout = function() {
+      auth.$logout();
+    }
+
+    $rootScope.$on("$firebaseSimpleLogin:login", function(e, user) {
+      $rootScope.currentUser = user;
+    });
+
+    $rootScope.$on("$firebaseSimpleLogin:logout", function() {
+      $rootScope.currentUser = null;
+    });
   }])
-  .controller('RegisterController', ['$scope', '$location', function($scope, $location) {
+  .controller('RegisterController', ['$scope', '$firebaseSimpleLogin', '$location', '$rootScope', function($scope, $firebaseSimpleLogin, $location, $rootScope) {
     $scope.name = 'Andrew';
+    var ref = new Firebase('https://domusstatic.firebaseio.com/');
+    var auth = $firebaseSimpleLogin(ref);
+    $scope.user = { email: '', password: '' };
+
     $scope.register = function() {
-      $location.path('/meetings');
+      auth.$createUser($scope.user.email, $scope.user.password).then(function(data) {
+        console.log(data);
+        auth.$login('password', $scope.user);
+        $location.path('/meetings');
+      });
     }
+
+    $rootScope.$on("$firebaseSimpleLogin:login", function(e, user) {
+      $rootScope.currentUser = user;
+    });
+
+    $rootScope.$on("$firebaseSimpleLogin:logout", function() {
+      $rootScope.currentUser = null;
+    });
+
   }])
   .controller('MeetingsController', ['$scope', '$firebase', function($scope, $firebase) {
     $scope.name = 'Tiny';
     var ref = new Firebase('https://domusstatic.firebaseio.com/meetings');
-    var meetings = $firebase(ref);
-    $scope.meetings = meetings.$asObject();
+    $scope.meetings = $firebase(ref);
+    // $scope.meetings = meetings.$asObject();
 
     $scope.addMeeting = function() {
       meetings.$push({
